67ec8bbd283fb564cbbaef0bc5ec092322fddbe0
fusethnd <fusethnd@gmail.com>
2024-03-08 10:06:00 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Thanadon Boontawee on 8/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
